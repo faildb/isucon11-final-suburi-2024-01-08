a5667be62d4d10497f13bcec17c10fb982143d28
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2024-01-08 10:47:52 +0900

    fix alp script

--- analyze.sh
+++ analyze.sh
@@ -24,9 +24,9 @@ sudo journalctl -e -nall -ocat -u openresty.service --since="${prepared_time}" >
 # alp
 # ALPM="/int/\d+,/uuid/[A-Za-z0-9_]+,/6digits/[a-z0-9]{6}"
 #ALPM="/@.+,/posts/\d+,/image/\d+.(jpg|png|gif),/posts?max_created_at.*$"
-#ALPM="/api/courses/[a-zA-Z0-9]+$,/api/courses/[a-zA-Z0-9]+/status,/api/courses/[a-zA-Z0-9]+/classes,/api/courses/[a-zA-Z0-9]+/classes/[a-zA-Z0-9]+/assignments,/api/courses/[a-zA-Z0-9]+/classes/[a-zA-Z0-9]+/assignments/scores,/api/courses/[a-zA-Z0-9]+/classes/[a-zA-Z0-9]+/assignments/export,/api/announcements/[a-zA-Z0-9]+$"
+ALPM="/api/courses/[a-zA-Z0-9]+$,/api/courses/[a-zA-Z0-9]+/status,/api/courses/[a-zA-Z0-9]+/classes,/api/courses/[a-zA-Z0-9]+/classes/[a-zA-Z0-9]+/assignments,/api/courses/[a-zA-Z0-9]+/classes/[a-zA-Z0-9]+/assignments/scores,/api/courses/[a-zA-Z0-9]+/classes/[a-zA-Z0-9]+/assignments/export,/api/announcements/[a-zA-Z0-9]+$"
 #ALPM="/initialize,/api/admin/clarifications,/api/admin/clarifications/\d,/api/session,/api/audience/teams,/api/audience/dashboard,/api/registration/session,/api/registration/team,/api/registration/contestant,/api/registration,/api/registration,/api/contestant/benchmark_jobs,/api/contestant/benchmark_jobs/\d,/api/contestant/clarifications,/api/contestant/clarifications,/api/contestant/dashboard,/api/contestant/notifications,/api/contestant/push_subscriptions,/api/contestant/push_subscriptions,/api/signup,/api/login,/api/logout"
-ALPM="/api/organizer/player/[a-zA-Z0-9-_]+/disqualified,/api/organizer/competition/[a-zA-Z0-9-_]+/finish,/api/organizer/competition/[a-zA-Z0-9-_]+/score,/api/player/player/[a-zA-Z0-9-_]+,/api/player/competition/[a-zA-Z0-9-_]+/ranking"
+#ALPM="/api/organizer/player/[a-zA-Z0-9-_]+/disqualified,/api/organizer/competition/[a-zA-Z0-9-_]+/finish,/api/organizer/competition/[a-zA-Z0-9-_]+/score,/api/player/player/[a-zA-Z0-9-_]+,/api/player/competition/[a-zA-Z0-9-_]+/ranking"
 
 echo -e "\n# ALPの集計結果"
 
