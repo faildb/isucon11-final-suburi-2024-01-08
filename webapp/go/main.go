f227ff9b72dc8bea477201b55cd5ebf23fd79ef8
mackee <macopy123@gmail.com>
2024-01-08 12:00:42 +0900

    dupエラーをpg向けにする

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -14,7 +14,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo-contrib/session"
@@ -26,11 +25,10 @@ import (
 )
 
 const (
-	SQLDirectory              = "../sql/pg/"
-	AssignmentsDirectory      = "../assignments/"
-	InitDataDirectory         = "../data/"
-	SessionName               = "isucholar_go"
-	mysqlErrNumDuplicateEntry = 1062
+	SQLDirectory         = "../sql/pg/"
+	AssignmentsDirectory = "../assignments/"
+	InitDataDirectory    = "../data/"
+	SessionName          = "isucholar_go"
 )
 
 type handlers struct {
@@ -857,7 +855,7 @@ func (h *handlers) AddCourse(c echo.Context) error {
 	_, err = h.DB.ExecContext(c.Request().Context(), "INSERT INTO `courses` (`id`, `code`, `type`, `name`, `description`, `credit`, `period`, `day_of_week`, `teacher_id`, `keywords`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
 		courseID, req.Code, req.Type, req.Name, req.Description, req.Credit, req.Period, req.DayOfWeek, userID, req.Keywords)
 	if err != nil {
-		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == uint16(mysqlErrNumDuplicateEntry) {
+		if pgxIsDuplicateError(err) {
 			var course Course
 			if err := h.DB.GetContext(c.Request().Context(), &course, "SELECT * FROM `courses` WHERE `code` = ?", req.Code); err != nil {
 				c.Logger().Error(err)
@@ -1069,7 +1067,7 @@ func (h *handlers) AddClass(c echo.Context) error {
 	if _, err := tx.ExecContext(c.Request().Context(), "INSERT INTO `classes` (`id`, `course_id`, `part`, `title`, `description`) VALUES (?, ?, ?, ?, ?)",
 		classID, courseID, req.Part, req.Title, req.Description); err != nil {
 		_ = tx.Rollback()
-		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == uint16(mysqlErrNumDuplicateEntry) {
+		if pgxIsDuplicateError(err) {
 			var class Class
 			if err := h.DB.GetContext(c.Request().Context(), &class, "SELECT * FROM `classes` WHERE `course_id` = ? AND `part` = ?", courseID, req.Part); err != nil {
 				c.Logger().Error(err)
@@ -1474,7 +1472,7 @@ func (h *handlers) AddAnnouncement(c echo.Context) error {
 	if _, err := tx.ExecContext(c.Request().Context(), "INSERT INTO `announcements` (`id`, `course_id`, `title`, `message`) VALUES (?, ?, ?, ?)",
 		req.ID, req.CourseID, req.Title, req.Message); err != nil {
 		_ = tx.Rollback()
-		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == uint16(mysqlErrNumDuplicateEntry) {
+		if pgxIsDuplicateError(err) {
 			var announcement Announcement
 			if err := h.DB.GetContext(c.Request().Context(), &announcement, "SELECT * FROM `announcements` WHERE `id` = ?", req.ID); err != nil {
 				c.Logger().Error(err)
